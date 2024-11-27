fea78a86ca8191a78ebf973c4d356586d0acb2e6
KimoDesgin <161530858+KimoDesgin@users.noreply.github.com>
2024-11-27 02:02:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ let m3dn = document.querySelector('.product2');
 let acc = document.querySelector('.product3');
 
 let productsOne = [
-    { id: 1, name: "  جبس أبيض كناوف 3 م", unit: "لوح", price: 310},
-    { id: 2, name: "  جبس أبيض كناوف 2.8 م", unit: "لوح", price: " "},
+    { id: 1, name: "  جبس أبيض كناوف 3 م", unit: "لوح", price: 325},
+    
     
     { id: 3, name: "  جبس أخضر كناوف ", unit: "لوح", price: 400},
-    { id: 4, name: "جبس أحمر كناوف", unit: "لوح", price: 400},
+    { id: 4, name: "جبس أحمر كناوف", unit: "لوح", price: 405},
     
-    { id: 5, name: "جبس أبيض جي بورد", unit: "لوح", price: 240},
+    { id: 5, name: "جبس أبيض جي بورد", unit: "لوح", price: 245},
     { id: 6, name: "جبس أخضر جي بورد", unit: "لوح", price: 345},
     { id: 6, name: "جبس أحمر جي بورد", unit: "لوح", price: 345}
+
+    { id: 2, name: "جبس أبيض عثمان ", unit: "لوح", price: 255},
 ];
 
 let productsTwo = [
@@ -46,9 +48,9 @@ let productsThree = [
     { id: 25, name: " مسمار 35 كيلو", unit: "علبة", price: 100},
     { id: 26, name: " مسمار 25 باتا", unit: "علبة", price: 135},
     { id: 27, name: " مسمار 35 فوكس", unit: "علبة", price: 145},
-    { id: 28, name: " مسمار تك", unit: "علبة", price: 150},
+    { id: 28, name: " مسمار تك", unit: "علبة", price: 140},
     { id: 29, name: " مسمار جنش", unit: "علبة", price: 60},
-    { id: 30, name: " أكمن", unit: "علبة", price: 120},
+    { id: 30, name: " أكمن", unit: "علبة", price: 125},
     { id: 31, name: " فيشر", unit: "علبة", price: 5},
     { id: 32, name: " تيش مصري", unit: "عود", price: 25},
     { id: 33, name: " تيش مستورد", unit: "عود", price: 30},
